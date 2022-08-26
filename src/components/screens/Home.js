04feb49222b0a2c1a375224df022eec6bbeea641
Saeed <s.najafi@mapiq.com>
2022-08-26 10:45:56 +0200

    adding new section

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -5,11 +5,16 @@ import {
   Text,
   TouchableOpacity,
   SafeAreaView,
+  Image,
+  ScrollView,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 
-function SettingsScreen({ text, photo }) {
+const text =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin. Suspendisse ultrices gravida dictum fusce. Quisque id diam vel quam. Consequat interdum varius sit amet mattis vulputate enim nulla. Tincidunt praesent semper feugiat nibh sed pulvina Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin. Suspendisse ultrices gravida dictum fusce. Quisque id diam vel quam. Consequat interdum varius sit amet mattis vulputate enim nulla. Tincidunt praesent semper feugiat nibh sed pulvina";
+
+function Section({ text }) {
   return (
     <View
       style={{
@@ -18,8 +23,12 @@ function SettingsScreen({ text, photo }) {
         alignItems: "center",
       }}
     >
-      <Text>{text}</Text>
-      <Image style={styles.tinyLogo} uri={photo} />
+      <Text style={styles.sectionStyle}>{text}</Text>
+
+      <Image
+        source={{ uri: "https://oasys-heal-dev.web.app/logo.png" }}
+        style={styles.tinyLogo}
+      />
     </View>
   );
 }
@@ -27,32 +36,37 @@ function SettingsScreen({ text, photo }) {
 function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style={styles.statusBar} />
-      <LinearGradient
-        colors={["#411a50", "#bb265f"]}
-        start={{ x: 0.1, y: 0.5 }}
-        end={{ x: 0.65, y: 0.65 }}
-        locations={[0.5, 0.5]}
-        style={styles.box}
-      >
-        <Text style={styles.titleText}>OasysNOW</Text>
-        <Text style={styles.bodyText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
-          tincidunt praesent semper feugiat nibh sed pulvinar proin. Suspendisse
-          ultrices gravida dictum fusce. Quisque id diam vel quam. Consequat
-          interdum varius sit amet mattis vulputate enim nulla. Tincidunt
-          praesent semper feugiat nibh sed pulvina
-        </Text>
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={[styles.buttonText, { color: "#411a50" }]}>
-            LET'S GET <Text style={styles.buttonText}>STARTED</Text>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.statusBar}>
+          <StatusBar style="light" />
+        </View>
+        <LinearGradient
+          colors={["#411a50", "#bb265f"]}
+          start={{ x: 0.3, y: 0.5 }}
+          end={{ x: 0.65, y: 0.65 }}
+          locations={[0.5, 0.5]}
+          style={styles.box}
+        >
+          <Text style={styles.titleText}>OasysNOW</Text>
+          <Text style={styles.bodyText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
+            tincidunt praesent semper feugiat nibh sed pulvinar proin.
+            Suspendisse ultrices gravida dictum fusce. Quisque id diam vel quam.
+            Consequat interdum varius sit amet mattis vulputate enim nulla.
+            Tincidunt praesent semper feugiat nibh sed pulvina
           </Text>
-        </TouchableOpacity>
-      </LinearGradient>
-      <View style={{ flex: 1 }}>
-        <Text style={[styles.titleText, { color: "black" }]}>ABOUT US</Text>
-      </View>
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text style={[styles.buttonText, { color: "#411a50" }]}>
+              LET'S GET <Text style={styles.buttonText}>STARTED</Text>
+            </Text>
+          </TouchableOpacity>
+        </LinearGradient>
+        <View style={styles.section}>
+          <Text style={[styles.titleText, { color: "black" }]}>ABOUT US</Text>
+          <Section text={text} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -63,10 +77,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  scrollView: {},
   box: {
     flex: 2,
     width: "100%",
     height: "100%",
+    paddingBottom: 20,
   },
   buttonStyle: {
     backgroundColor: "#fff",
@@ -96,12 +112,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   tinyLogo: {
-    width: 50,
-    height: 50,
+    width: 150,
+    height: 150,
   },
-  statusBar: {
-    marginTop: 50,
+  sectionStyle: {
+    padding: 20,
+    marginTop: 20,
   },
+  section: { flex: 1, marginTop: 20, marginBottom: 20 },
+  statusBar: { paddingTop: 20 },
 });
 
 export default Home;
